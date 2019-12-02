7bb4664abaa58f52742eead5a944bbf11a7dc30b
Kevin Bonham <kevbonham@gmail.com>
2019-12-02 17:28:05 -0500

    2b finalize

--- day2.jl
+++ day2.jl
@@ -1,5 +1,4 @@
 using Test
-using Combinatorics
 
 function getinstruction(mem, address)
     return mem[address .+ [0,1,2,3]]
@@ -28,11 +27,6 @@ function processmemory!(mem)
     return mem
 end
 
-@test processmemory!([1,0,0,0,99]) == [2,0,0,0,99]
-@test processmemory!([2,3,0,3,99]) == [2,3,0,6,99]
-@test processmemory!([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
-@test processmemory!([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
-
 function startup(infile)
     return parse.(Int, split(readlines(infile)[1], ","))
 end
@@ -43,26 +37,14 @@ function setmem!(mem, noun, verb)
     return mem
 end
 
-function solution1(infile)
-    mem = startup(infile)
-    setmem!(mem, 12, 2)
-    # run
-    processmemory!(mem)
-    return mem[1]
-end
-
-solution1("inputs/input2.txt")
-
-
 function solution2(infile)
-    mem = startup(infile)
-    for (noun, verb) in with_replacement_combinations(0:99, 2)
-        workingmem = deepcopy(mem)
-        setmem!(workingmem, noun, verb)
-        processmemory!(workingmem)
-        workingmem[1] == 19690720 && return (100 * noun + verb)
+    for (noun, verb) in Iterators.product(0:99, 0:99)
+        mem = startup(infile)
+        setmem!(mem, noun, verb)
+        processmemory!(mem)
+        mem[1] == 19690720 && return (100 * noun + verb)
     end
     error("Didn't find a noun/verb combo")
 end
 
-solution2("inputs/input2.txt")
+@test solution2("inputs/input2.txt") == 6421
