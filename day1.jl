11e382783709c1a4345d24d1213a407f968a59d0
Kevin Bonham <kevbonham@gmail.com>
2019-12-01 21:40:38 -0500

    1b first attempt

--- day1.jl
+++ day1.jl
@@ -1,5 +1,7 @@
 function fuelcost(mass)
-    return floor(mass/3) - 2
+    cost = floor(mass/3) - 2
+    cost < 0 && (cost = 0.)
+    return cost
 end
 
 function solution1(infile)
@@ -8,5 +10,25 @@ function solution1(infile)
         mass = parse(Float64, line)
         cost += fuelcost(mass)
     end
-    return cost
+    return Int(cost)
 end
+
+solution1("inputs/input1.1")
+
+
+function solution2(infile)
+    cost = 0
+    for line in eachline(infile)
+        mass = parse(Float64, line)
+        cost += fuelcost(mass)
+    end
+
+    fcost = fuelcost(cost)
+    while fcost > 0
+        cost += fcost
+        fcost = fuelcost(fcost)
+    end
+    return Int(cost)
+end
+
+solution2("inputs/input1.1")
