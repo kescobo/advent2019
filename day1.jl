8c65555d7f5c10b0090df7679c016a8909000785
Kevin Bonham <kevbonham@gmail.com>
2019-12-01 21:44:55 -0500

    1b solution

--- day1.jl
+++ day1.jl
@@ -15,19 +15,25 @@ end
 
 solution1("inputs/input1.1")
 
-
-function solution2(infile)
-    cost = 0
-    for line in eachline(infile)
-        mass = parse(Float64, line)
-        cost += fuelcost(mass)
-    end
-
+function recursivecost(mass)
+    cost = fuelcost(mass)
     fcost = fuelcost(cost)
     while fcost > 0
         cost += fcost
         fcost = fuelcost(fcost)
     end
+    return cost
+end
+
+recursivecost(100756)
+
+function solution2(infile)
+    cost = 0
+    for line in eachline(infile)
+        mass = parse(Float64, line)
+        cost += recursivecost(mass)
+    end
+
     return Int(cost)
 end
 
