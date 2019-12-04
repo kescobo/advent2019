13b6f38d7b5a8ae45a3bda9b36af8e7ac3b0f829
Kevin Bonham <kevbonham@gmail.com>
2019-12-04 13:36:44 -0500

    solution 3b

--- day3.jl
+++ day3.jl
@@ -14,37 +14,39 @@ end
 
 move(source, wp::WirePath) = error("Invalid Path")
 
-move(source::CartesianIndex, wp::WirePath{:U}) = source + CartesianIndex(0,wp.n)
-move(source::CartesianIndex, wp::WirePath{:D}) = source - CartesianIndex(0,wp.n)
-move(source::CartesianIndex, wp::WirePath{:R}) = source + CartesianIndex(wp.n,0)
-move(source::CartesianIndex, wp::WirePath{:L}) = source - CartesianIndex(wp.n,0)
+move(source, wp::WirePath{:U}) = source .+ (0,wp.n)
+move(source, wp::WirePath{:D}) = source .- (0,wp.n)
+move(source, wp::WirePath{:R}) = source .+ (wp.n,0)
+move(source, wp::WirePath{:L}) = source .- (wp.n,0)
 
 function straightpath(source, dest)
-    return union(
-        CartesianIndices(
-            range.(Tuple(source), Tuple(dest))),
-        CartesianIndices(
-            range.(Tuple(dest), Tuple(source))),
-        )
+    if source[2] == dest[2]
+        source[1] < dest[1] ? xs = range(source[1], stop=dest[1]) : xs = range(source[1], step=-1, stop=dest[1])
+        ys = fill(source[2], length(xs))
+    elseif source[1] == dest[1]
+        source[2] < dest[2] ? ys = range(source[2], stop=dest[2]) : ys = range(source[2], step=-1, stop=dest[2])
+        xs = fill(source[1], length(ys))
+    else
+        @error "No straight path available" source, dest
+    end
+
+    return zip(xs[2:end],ys[2:end])
 end
 
 function wirepath(paths)
-    points = Set{CartesianIndex}([])
-    source = CartesianIndex(0,0)
+    points = [(0,0)]
     for p in WirePath.(paths)
+        source = points[end]
         dest = move(source, p)
-        newpoints = straightpath(source, dest)
-        points = union(points, newpoints)
-        source = dest
+        append!(points, straightpath(source, dest))
     end
-    pop!(points, CartesianIndex(0,0))
-    return points
+    return points[2:end]
 end
 
 
 function crosswires(w1, w2)
     crosses = intersect(wirepath(w1), wirepath(w2)) |> collect
-    return minimum(map(c-> sum(abs(i) for i in Tuple(c)), crosses))
+    return minimum(map(c-> sum(abs(i) for i in c), crosses))
 end
 
 
@@ -56,7 +58,38 @@ end
                  ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]) == 135
 
 function solution1(infile)
-    (w1,w2) = map(l-> split(l, ","), eachline(infile))
-    crosswires(String.(w1), String.(w1))
+    wires = map(l-> split(l, ","), eachline(infile))
+    crosswires(wires[1], wires[2])
 end
+
 solution1("inputs/input3.txt")
+
+function crosspaths(w1, w2)
+    p1, p2 = (wirepath(w1), wirepath(w2))
+    l1, l2 = (length(p1), length(p2))
+    crosses = Dict()
+    for i in 1:max(l1, l2)
+        if i <= l1 && p1[i] in p2
+            haskey(crosses, p1[i]) ? crosses[p1[i]]["p1"] = i : crosses[p1[i]] = Dict("p1"=>i)
+        end
+        if i <= l2 && p2[i] in p1
+            haskey(crosses, p2[i]) ? crosses[p2[i]]["p2"] = i : crosses[p2[i]] = Dict("p2"=>i)
+        end
+    end
+    return minimum(sum((crosses[k]["p1"], crosses[k]["p2"])) for k in keys(crosses))
+    # return crosses
+end
+
+@test crosspaths(["R8","U5","L5","D3"], ["U7","R6","D4","L4"]) == 30
+
+@test crosspaths(["R75","D30","R83","U83","L12","D49","R71","U7","L72"],
+                 ["U62","R66","U55","R34","D71","R55","D58","R83"]) == 610
+@test crosspaths(["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],
+                 ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]) == 410
+
+function solution2(infile)
+    wires = map(l-> split(l, ","), eachline(infile))
+    crosspaths(wires[1], wires[2])
+end
+
+solution2("inputs/input3.txt")
